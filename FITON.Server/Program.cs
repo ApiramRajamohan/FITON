54ace0ebbc4238346bb3d971d2e45b0b0b116fc6
ApiramR <it23444782@my.sliit.lk>
2025-09-26 21:55:33 +0530

    inmemory database added

--- FITON.Server/Program.cs
+++ FITON.Server/Program.cs
@@ -7,8 +7,10 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseInMemoryDatabase("FitonDevDB"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
