0bc45bedfde7c84390120677889d0ae09d525b14
ApiramR <it23444782@my.sliit.lk>
2025-10-01 09:50:09 +0530

    update issue resolved

--- FITON.Server/Program.cs
+++ FITON.Server/Program.cs
@@ -7,10 +7,9 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseInMemoryDatabase("FitonDevDB"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddAuthentication(options =>
 {
